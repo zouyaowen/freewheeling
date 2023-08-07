f62eb23ec3e344935c1f93381c331d1a299a0c96
zouyaowen <zouyaowen@bytedance.com>
2023-08-08 00:25:12 +0800

    feat: push

---
+++ .vitepress/config.ts
@@ -0,0 +1,86 @@
+import {defineConfig} from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+    title: "FollowYourHeart",
+    description: "LearningNote",
+    themeConfig: {
+        // https://vitepress.dev/reference/default-theme-config
+        nav: [
+            {text: 'Home', link: '/'},
+            {text: 'Examples', link: '/markdown-examples'},
+            {text: 'RuntimeAPI', link: '/api-examples'},
+            {text: 'EnvLearning', link: '/env'},
+            {text: 'Guide', link: '/guide/'},
+            {text: 'Config', link: '/config/'}
+        ],
+
+        // sidebar: [
+        //   {
+        //     text: 'Examples',
+        //     collapsable: false,
+        //     items: [
+        //       { text: 'Markdown Examples', link: '/markdown-examples' },
+        //       { text: 'Runtime API Examples', link: '/api-examples' }
+        //     ]
+        //   },
+        //   {
+        //     text: 'EnvStudy',
+        //     collapsable: false,
+        //     items: [
+        //       { text: 'LinuxEnv', link: '/env' }
+        //     ]
+        //   }
+        // ],
+        sidebar: {
+            // This sidebar gets displayed when a user
+            // is on `guide` directory.
+            "/": [
+                {
+                    text: 'Examples',
+                    items: [
+                        {text: 'Markdown Examples', link: '/markdown-examples'},
+                        {text: 'Runtime API Examples', link: '/api-examples'}
+                    ]
+                },
+                {
+                    text: 'EnvStudy',
+                    items: [
+                        {text: 'LinuxEnv', link: '/env'}
+                    ]
+                }
+            ],
+            '/guide/': [
+                {
+                    text: 'Guide',
+                    items: [
+                        {text: 'Index', link: '/guide/'},
+                        {text: 'One', link: '/guide/one'},
+                        {text: 'Two', link: '/guide/two'}
+                    ]
+                }
+            ],
+
+            // This sidebar gets displayed when a user
+            // is on `config` directory.
+            '/config/': [
+                {
+                    text: 'Config',
+                    items: [
+                        {text: 'Index', link: '/config/'},
+                        {text: 'Three', link: '/config/three'},
+                        {text: 'Four', link: '/config/four'}
+                    ]
+                }
+            ]
+        },
+
+        socialLinks: [
+            {icon: 'github', link: 'https://github.com/vuejs/vitepress'}
+        ],
+        footer: {
+            message: '<a href="https://beian.miit.gov.cn/" target="_blank">皖ICP备18026052号-2</a>',
+            copyright: '关注公众号：北国故事'
+        }
+    }
+})
